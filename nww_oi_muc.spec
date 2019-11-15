9d960b69aa0375cf75e66c473ebc26639bf78e74
warrickmoran <warrick.moran@noaa.gov>
2019-11-15 10:22:19 -0500

    Add files via upload

---
+++ nww_oi_muc.spec
@@ -0,0 +1,33 @@
+# -*- mode: python ; coding: utf-8 -*-
+
+block_cipher = None
+
+
+a = Analysis(['../nww_oi_muc.py', '../nww_oi_muc_bot.py', '../nww_oi_rate.py'],
+             pathex=['/home/warrick.moran/eclipse-ml/nww_oi'],
+             binaries=[],
+             datas=[],
+             hiddenimports=['sleekxmpp.features', 'sleekxmpp.features.feature_starttls', 'sleekxmpp.features.feature_bind', 'sleekxmpp.features.feature_session', 'sleekxmpp.features.feature_bind', 'sleekxmpp.features.feature_rosterver', 'sleekxmpp.features.feature_mechanisms', 'sleekxmpp.features.feature_preapproval', 'sleekxmpp.plugins.xep_0004', 'sleekxmpp.plugins.xep_0030', 'sleekxmpp.plugins.xep_0045', 'sleekxmpp.plugins.xep_0199'],
+             hookspath=[],
+             runtime_hooks=[],
+             excludes=[],
+             win_no_prefer_redirects=False,
+             win_private_assemblies=False,
+             cipher=block_cipher,
+             noarchive=False)
+pyz = PYZ(a.pure, a.zipped_data,
+             cipher=block_cipher)
+exe = EXE(pyz,
+          a.scripts,
+          a.binaries,
+          a.zipfiles,
+          a.datas,
+          [],
+          name='nww_oi_muc',
+          debug=False,
+          bootloader_ignore_signals=False,
+          strip=False,
+          upx=True,
+          upx_exclude=[],
+          runtime_tmpdir=None,
+          console=True )
---
+++ nww_oi_rate.py
@@ -0,0 +1,66 @@
+'''
+Created on Nov 8, 2019
+
+@author: warrick.moran
+'''
+import nww_oi_muc_bot as MUCBOT
+import datetime
+import logging
+import socket
+import numpy as np
+from threading import Timer
+
+# Create a custom logger
+logger = logging.getLogger(__name__)
+
+
+class OIMetrics_Rate(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, muc: MUCBOT, interval=5):
+        '''
+        Constructor
+        '''
+        self.avg = None
+        self.muc = muc
+        self.interval = interval * 60
+        self.start_time = datetime.datetime.now()
+        
+        self.timer = Timer(self.interval, self.calculate, ())
+        self.timer.start()
+        self.xs = []
+        self.ys = []
+    
+    def calculate(self):
+        timenow = datetime.datetime.now()
+        timediff = (timenow - self.start_time)
+        timediff_min = int(timediff.seconds / 60)
+        average = self.muc.product_count / timediff_min
+        
+        logger.debug("Rate Calculate: Member List-{}, TimeDiff-{}, TimeDiff Min-{}, Avg-{}".format(len(self.muc.member_list),timediff, timediff_min, average))
+    
+        if (self.avg is None):
+            self.avg = np.array([[timediff_min,average,len(self.muc.member_list),socket.gethostbyname(self.muc.url)]])
+        else:
+            self.avg = np.append(self.avg, [[timediff_min,average, len(self.muc.member_list),socket.gethostbyname(self.muc.url)]],axis=0)
+            
+        self.reset()
+            
+    def reset(self):
+        if (self.timer.isAlive()):
+            self.timer.cancel()
+        self.timer = Timer(self.interval, self.calculate, ())
+        self.timer.start()
+        
+    def start(self):
+        self.reset()
+        self.init()
+        self.timer.start()
+        
+    def init(self):
+        if (self.avg is not None):
+            del self.avg
+        
