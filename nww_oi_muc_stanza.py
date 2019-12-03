f770b6cebf75808c4dc03c0c54b915c9d67d9c52
warrickmoran <warrick.moran@noaa.gov>
2019-12-03 14:41:04 -0500

    add nwws-oi custom stanza

---
+++ nww_oi_muc_stanza.py
@@ -0,0 +1,14 @@
+'''
+Created on Dec 3, 2019
+
+@author: warrick.moran
+'''
+from sleekxmpp.xmlstream.stanzabase import ElementBase
+
+class X(ElementBase):
+    namespace = 'nwws-oi'
+    name = 'x'
+    plugin_attrib = 'x'
+    interfaces = set(('cccc', 'ttaaii', 'issue', 'awipsid', 'id'))
+    sub_interfaces = interfaces
+        
